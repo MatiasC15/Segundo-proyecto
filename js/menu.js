121916ded091b0f5d730def2684d4772b5b3579f
Matias Cordoba <matiascordoba79@gmail.com>
2023-04-08 15:55:35 -0300

    pagina principal y carrito funcionales y responsive

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+const openMenu = document.querySelector(".open_menu");
+const closeMenu = document.querySelector(".close_menu");
+const aside = document.querySelector("aside");
+
+openMenu.addEventListener("click", () => {
+    aside.classList.add("aside_visible")
+})
+closeMenu.addEventListener("click", () => {
+    aside.classList.remove("aside_visible")
+})
+
+categoriesButtons.forEach(button => button.addEventListener("click", () => {
+    aside.classList.remove("aside_visible")
+}))
+
