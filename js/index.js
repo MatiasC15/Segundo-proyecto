47e41ad6b2e34b222434849e4c585a22fa301cc1
Matias Cordoba <matiascordoba79@gmail.com>
2023-04-07 21:52:38 -0300

    main y carrito terminado, falta hacerlos responsive

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 let products = [
   {
     id: "periferico-01",
-    name: "Auriculares HyperX Cloud 7.1",
+    name: "Auriculares HyperX Cloud",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_30992_Auriculares_HyperX_Cloud_Revolver_7.1_3dd11be0-grn.jpg",
     category: {
       name: "Perifericos",
       id: "perifericos",
     },
-    price: "$ 35.380",
-    stock: "12"
+    price: "35380",
+    stock: "12",
   },
 
   {
@@ -21,20 +21,20 @@ let products = [
       name: "Monitores",
       id: "monitores",
     },
-    price: "$ 48.250",
-    stock: "6"
+    price: "48250",
+    stock: "6",
   },
   {
     id: "monitor-02",
-    name: "Monitor LG LCD 49 49WL95C Curvo ",
+    name: "Monitor LG LCD 49",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_30683_Monitor_LG_LCD_49__49WL95C_Curvo_Ultra_Wide_DUAL_QHD_b4c30ae3-grn.jpg",
     category: {
       name: "Monitores",
       id: "monitores",
     },
-    price: "$ 602.999",
-    stock: "4"
+    price: "602999",
+    stock: "4",
   },
   {
     id: "monitor-03",
@@ -45,8 +45,8 @@ let products = [
       name: "Monitores",
       id: "monitores",
     },
-    price: "$ 433.160",
-    stock: "4"
+    price: "433160",
+    stock: "4",
   },
   {
     id: "monitor-04",
@@ -57,20 +57,20 @@ let products = [
       name: "Monitores",
       id: "monitores",
     },
-    price: "$ 197.750",
-    stock:"12"
+    price: "197750",
+    stock: "12",
   },
   {
     id: "monitor-05",
-    name: "Monitor Gamer LG 27 144hz IPS FHD",
+    name: "Monitor Gamer LG 27 144hz",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_19484_Monitor_Gamer_LG_27__27GL650F_144hz_IPS_FHD_GSync_FreeSync_1e30b9f9-grn.jpg",
     category: {
       name: "Monitores",
       id: "monitores",
     },
-    price: "$ 158.800",
-    stock: "8"
+    price: "158800",
+    stock: "8",
   },
   {
     id: "audio-01",
@@ -81,68 +81,68 @@ let products = [
       name: "Audio",
       id: "audio",
     },
-    price: "$ 44.500",
-    stock: "14"
+    price: "44500",
+    stock: "14",
   },
   {
     id: "audio-02",
-    name: "Parlantes Logitech Z607 5.1",
+    name: "Parlantes Logitech Z607",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_13515_Parlantes_Logitech_Z607_5.1_Surround_Bluetooth_160W__33f76a80-grn.jpg",
     category: {
       name: "Audio",
       id: "audio",
     },
-    price: "$ 57.150",
-    stock:"3"
+    price: "57150",
+    stock: "3",
   },
   {
     id: "audio-03",
-    name: "Barra de Sonido Redragon GS570 RGB",
+    name: "Parlante Redragon GS570 ",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_33485_Barra_de_Sonido_Redragon_GS570_Darknet_RGB_7de1cdbd-grn.jpg",
     category: {
       name: "Audio",
       id: "audio",
     },
-    price: "$ 13.990",
-    stock:"14"
+    price: "13990",
+    stock: "14",
   },
   {
     id: "audio-04",
-    name: "Parlantes Logitech Bluetooth G560 2.1 RGB",
+    name: "Parlantes Logitech G560",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_17309_Parlantes_Logitech_Bluetooth_G560_2.1_RGB_Lightsync_240W_7ed24541-grn.jpg",
     category: {
       name: "Audio",
       id: "audio",
     },
-    price: "$ 104.999",
-    stock:"3"
+    price: "104999",
+    stock: "3",
   },
   {
     id: "silla-01",
-    name: "Silla Gamer Shenlong SCH-RGB155 Black",
+    name: "Silla Gamer Shenlong Black",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_36129_Silla_Gamer_Shenlong_SCH-RGB155_Black_7347c61c-grn.jpg",
     category: {
       name: "Sillas",
       id: "sillas",
     },
-    price: "$ 97.250",
-    stock: "7"
+    price: "97250",
+    stock: "7",
   },
   {
     id: "silla-02",
-    name: "Silla gamers Corsair T1 Race Black",
+    name: "Silla gamers Corsair T1",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_6000_Silla_Gamer_Corsair_T1_Race_Black_Red__5df97cb1-grn.jpg",
     category: {
       name: "Sillas",
       id: "sillas",
     },
-    price: "$ 124.560",
-    stock:"8"
+    price: "124560",
+    stock: "8",
   },
   {
     id: "componente-01",
@@ -153,68 +153,68 @@ let products = [
       name: "Componentes",
       id: "componentes",
     },
-    price: "$ 35.950",
-    stock:"56"
+    price: "35950",
+    stock: "56",
   },
   {
     id: "componente-02",
-    name: "Fuente ASUS TUF 80 plus Bronze",
+    name: "Fuente ASUS TUF 80+ B",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_23310_Fuente_ASUS_TUF_550W_80_Plus_Bronze_550B_cfcb396e-grn.jpg",
     category: {
       name: "Componentes",
       id: "componentes",
     },
-    price: "$ 33.050",
-    stock:"26"
+    price: "33050",
+    stock: "26",
   },
   {
     id: "componente-03",
-    name: "Fuente ASUS ROG 850W 80+ Platinum",
+    name: "Fuente ASUS ROG 850W 80+ P",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_23295_Fuente_ASUS_ROG_THOR_850W_80_Plus_Platinum_850P_Full_modular_4de04ae6-grn.jpg",
     category: {
       name: "Componentes",
       id: "componentes",
     },
-    price: "$ 46.850",
-    stock:"42"
+    price: "46850",
+    stock: "42",
   },
   {
     id: "componente-04",
-    name: "Cooler CPU Thermaltake UX1000 ARGB",
+    name: "Cooler CPU Thermaltake UX1000",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_33637_Cooler_CPU_Thermaltake_UX100_ARGB_12a77695-grn.jpg",
     category: {
       name: "Componentes",
       id: "componentes",
     },
-    price: "$ 7.860",
-    stock:"56"
+    price: "7860",
+    stock: "56",
   },
   {
     id: "componente-05",
-    name: "Procesador AMD RYZEN3 3200G 4.O GHz",
+    name: "Procesador AMD RYZEN3 3200G",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_16752_Procesador_AMD_RYZEN_3_3200G_4.0GHz_Turbo___Radeon_Vega_8_AM4_Wraith_Stealth_Cooler_f414a507-grn.jpg",
     category: {
       name: "Componentes",
       id: "componentes",
     },
-    price: "$ 79.860",
-    stock:"40"
+    price: "79860",
+    stock: "40",
   },
   {
     id: "componente-06",
-    name: "Procesador AMD Ryzen7 5700G 4.6GHz ",
+    name: "Procesador AMD Ryzen7 5700G",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_27673_Procesador_AMD_Ryzen_7_5700G_4.6GHz_Turbo___Wraith_Stealth_Cooler_cb33e4fa-grn.jpg",
     category: {
       name: "Componentes",
       id: "componentes",
     },
-    price: "$ 109.200",
-    stock:"32"
+    price: "109200",
+    stock: "32",
   },
   {
     id: "periferico-02",
@@ -225,44 +225,44 @@ let products = [
       name: "Perifericos",
       id: "perifericos",
     },
-    price: "$ 39.510",
-    stock:"22"
+    price: "39510",
+    stock: "22",
   },
   {
     id: "periferico-03",
-    name: "Mouse Logitech G502 Lightspeed RGB",
+    name: "Mouse Logitech G502",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_16569_Mouse_Logitech_G502_Lightspeed_Wireless_RGB_Lightsync_f98d2f40-grn.jpg",
     category: {
       name: "Perifericos",
       id: "perifericos",
     },
-    price: "$ 31.340",
-    stock:"24"
+    price: "31340",
+    stock: "24",
   },
   {
     id: "componente-07",
-    name: "Mother Asrock X670E Taichi Carrara AM5",
+    name: "Mother Asrock X670E",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_34911_Mother_Asrock_X670E_Taichi_Carrara_AM5_00f2f1c2-grn.jpg",
     category: {
       name: "Componentes",
       id: "componentes",
     },
-    price: "$ 313.220",
-    stock:"22"
+    price: "313220",
+    stock: "22",
   },
   {
     id: "componente-08",
-    name: "Mother Asrock X670E Steel Legend AM5",
+    name: "Mother Asrock X670E Steel",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_34460_Mother_Asrock_X670E_Steel_Legend_AM5_75fc36a9-grn.jpg",
     category: {
       name: "Componentes",
       id: "componentes",
     },
-    price: "$ 219.200",
-    stock:"25"
+    price: "219200",
+    stock: "25",
   },
   {
     id: "periferico-04",
@@ -273,42 +273,42 @@ let products = [
       name: "Perifericos",
       id: "perifericos",
     },
-    price: "$ 31.020",
-    stock:"28"
+    price: "31020",
+    stock: "28",
   },
   {
     id: "componente-09",
-    name: "Gabinete Kolink Inspire K3 RGB",
+    name: "Gabinete Kolink Inspire K3",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_34567_Gabinete_Kolink_Inspire_K3_RGB__M-ATX_Vidrio_Templado_7306ef99-grn.jpg",
     category: {
       name: "Componentes",
       id: "componentes",
     },
-    price: "$ 26.550",
-    stock:"12"
+    price: "26550",
+    stock: "12",
   },
   {
     id: "componente-10",
-    name: "Gabinete Lian Li Odyssey X Silver",
+    name: "Gabinete Lian Li Odyssey",
     image:
       "https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_32695_Gabinete_Lian_Li_Odyssey_X_Silver_d754dc77-grn.jpg",
     category: {
       name: "Componentes",
       id: "componentes",
     },
-    price: "$ 244.100",
-    stock:"16"
+    price: "244100",
+    stock: "16",
   },
 ];
 
 const productsContainer = document.querySelector("#products_container");
 const categoriesButtons = document.querySelectorAll(".btn_category");
 const principalTitle = document.querySelector("#principal_title");
-let addButton = document.querySelectorAll(".add_product")
+let addButton = document.querySelectorAll(".add_product");
+const numberOfProducts = document.querySelector("#number_of_products");
 
 function chargeProducts(chosenProducts) {
-
   productsContainer.innerHTML = "";
 
   chosenProducts.forEach((product) => {
@@ -321,53 +321,81 @@ function chargeProducts(chosenProducts) {
                     />
                     <div class="product_info_container">
                        <h3 class="product_name">${product.name}</h3>
-                       <p class="product_price">${product.price}</p>
+                       <p class="product_price">$ ${product.price}</p>
                        <button class="add_product" id="${product.id}">Agregar al carrito</button>
                     </div>`;
 
     productsContainer.append(divProduct);
   });
-  updateAddButtons()
+  updateAddButtons();
 }
 
 chargeProducts(products);
 
-categoriesButtons.forEach(button => {
+categoriesButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     categoriesButtons.forEach((button) => button.classList.remove("active"));
 
     e.currentTarget.classList.add("active");
 
-    if(e.currentTarget.id != "all_products") {
-      const categoriesTitle = products.find(product => product.category.id === e.currentTarget.id);
-      principalTitle.innerText = categoriesTitle.category.name
+    if (e.currentTarget.id != "all_products") {
+      const categoriesTitle = products.find(
+        (product) => product.category.id === e.currentTarget.id
+      );
+      principalTitle.innerText = categoriesTitle.category.name;
 
-        const productsByCategory = products.filter(
+      const productsByCategory = products.filter(
         (product) => product.category.id === e.currentTarget.id
       );
       chargeProducts(productsByCategory);
-    }else{ 
-      principalTitle.innerText = "Todos los productos"
+    } else {
+      principalTitle.innerText = "Todos los productos";
       chargeProducts(products);
     }
   });
 });
 
-function updateAddButtons(){
+function updateAddButtons() {
   addButton = document.querySelectorAll(".add_product");
 
-  addButton.forEach(button => {
-    button.addEventListener("click", addCart)
-  })
+  addButton.forEach((button) => {
+    button.addEventListener("click", addCart);
+  });
 }
+let productsInCart;
+const productsInCartLS = localStorage.getItem("products");
 
-const productsInCart = [];
+if(productsInCartLS){
+  productsInCart = JSON.parse(productsInCartLS);
+  updateNumberOfProducts();
+}else{
+  productsInCart = [];
+} 
+
 
 function addCart(e) {
- const idButton = e.currentTarget.id;
- const addedProducts = products.find(product => product.id === idButton);
+  const idButton = e.currentTarget.id;
+  const addedProduct = products.find((product) => product.id === idButton);
 
- productsInCart.push(addedProducts);
- console.log(productsInCart)
+  if (productsInCart.some((product) => product.id === idButton)) {
+    const index = productsInCart.findIndex(
+      (product) => product.id === idButton
+    );
+    productsInCart[index].quantity++;
+  } else {
+    addedProduct.quantity = 1;
+    productsInCart.push(addedProduct);
+  }
+  updateNumberOfProducts();
+
+  
+  localStorage.setItem("products", JSON.stringify(productsInCart));
+}
+
+function updateNumberOfProducts() {
+  let newNumber = productsInCart.reduce(
+    (acc, product) => acc + product.quantity,0);
+
+  numberOfProducts.innerText = newNumber;
 }
 
